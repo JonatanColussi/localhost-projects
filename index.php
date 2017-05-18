6169207f0075248863e7a6e6470cc0a0ce45070c
Jonatan Colussi <jcolussi@reweb.com.br>
2017-05-18 14:42:45 -0300

    Projeto Inicial

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Jonatan Colussi">
+    <title>Gerenciador de projetos | Jonatan Colussi</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <div class="container">
+    	<div class="row">
+    		<div class="col-md-12">
+	    		<h1 class="title">Gerenciador de projetos | Jonatan Colussi</h1>
+    		</div>
+    	</div>
+    	<div class="row">
+    		<div class="col-md-12">
+	    		<div class="form-group box-search">
+	    			<input type="search" name="filter" placeholder="Pesquisar" class="form-control">
+	    		</div>
+    		</div>
+    	</div>
+		<div class="row projects">
+			<?php 
+				foreach(glob('../*', GLOB_ONLYDIR) as $project){
+					$nameProject = str_replace('../', null, $project);
+
+					if(strrpos($nameProject, '-') != false)
+						$name = explode('-', $nameProject);
+					elseif(strrpos($nameProject, '_') != false)
+						$name = explode('_', $nameProject);
+					else
+						$name = [$nameProject];
+
+					$name = array_map('ucfirst', $name);
+
+					$nameProject = implode(' ', $name);
+
+					$nameProject = trim($nameProject);
+
+					echo "<div data-url=\"{$project}\">
+								<span>{$nameProject}</span>
+							</div>";
+				}
+			?>
+		</div>
+
+
+    </div>
+
+    <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="js/scripts.js"></script>
+  </body>
+</html>
---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function($) {
+	$("[name=filter]").on('keydown keyup', function(){
+		var pesquisa = $(this).val();
+		pesquisa = pesquisa.toUpperCase();
+		$(".projects > div").each(function(){
+			if($(this).text().toUpperCase().indexOf(pesquisa) >= 0)
+				$(this).show('fast');
+			else
+				$(this).hide('fast');
+		});
+	});
+	$(".projects > div").on('click', function(){
+		event.preventDefault();
+		window.location.href = $(this).data('url');
+	});
+});
