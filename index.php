6843a61746303e28c64e707fb3aa5a7068e8d691
Jonatan Colussi <jcolussi@reweb.com.br>
2017-05-19 14:44:03 -0300

    Install default

--- index.php
+++ index.php
@@ -1,71 +1,10 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="Jonatan Colussi">
-    <title>Gerenciador de projetos | Jonatan Colussi</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/font-awesome.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
-  </head>
-
-  <body>
-    <main class="container">
-    	<div class="row">
-    		<div class="col-md-12">
-	    		<h1 class="title">Gerenciador de projetos | Jonatan Colussi</h1>
-    		</div>
-    	</div>
-    	<div class="row">
-    		<div class="col-md-12">
-	    		<div class="form-group box-search">
-	    			<input type="search" name="filter" placeholder="Pesquisar" class="form-control" autofocus>
-	    		</div>
-    		</div>
-    	</div>
-		<div class="row projects">
-			<?php
-				foreach(glob('../*', GLOB_ONLYDIR) as $project){
-                    $subdirs = glob($project.'/{,.}*', GLOB_BRACE);
-                
-                    $hasGit = in_array($project.'/.git', $subdirs);
-                    $hasSftp = in_array($project.'/sftp-config.json', $subdirs);
-
-                    $nameProject = str_replace('../', null, $project);
-
-                    if(strrpos($nameProject, '-') != false)
-                        $name = explode('-', $nameProject);
-                    elseif(strrpos($nameProject, '_') != false)
-                        $name = explode('_', $nameProject);
-                    else
-                        $name = [$nameProject];
-
-                    $name = array_map('ucfirst', $name);
-
-                    $nameProject = implode(' ', $name);
-
-                    $nameProject = trim($nameProject);
-					echo "<div data-url=\"{$project}\">
-                                <div>";
-                    if($hasGit) echo "<i class=\"fa fa-code-fork\" title=\"É um repositório Git\"></i>";
-                    if($hasSftp) echo "<i class=\"fa fa-server\" title=\"Contém os dados de ftp\"></i>";
-                            echo "</div>
-                                <div>
-								    <span>{$nameProject}</span>
-                                </div>
-							</div>";
-				}
-			?>
-		</div>
-    </main>
-
-    <footer>
-        <?= date('Y') ?> Jonatan Colussi
-    </footer>
-
-    <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
-    <script type="text/javascript" src="js/scripts.js"></script>
-  </body>
-</html>
+<?php
+	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+		$uri = 'https://';
+	} else {
+		$uri = 'http://';
+	}
+	$uri .= $_SERVER['HTTP_HOST'];
+	header('Location: '.$uri.'/gerenciador-projetos/');
+	exit;
+?>
